51153ef7df5fb2819b2e098893961e6a357f3642
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-05-10 22:23:05 +1200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -108,7 +108,7 @@ function Boot() {
   return (
     <PrivyProvider
       appId="cma4m3v6400dkl10ld7792jm2"
-      walletConnectProjectId={false}
+      config={{ walletConnectCloudProjectId: undefined}}
     >
       <AuthenticatedApp />
     </PrivyProvider>
