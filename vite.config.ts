e39cae25f34cc12881906499cfecd5e3f88e89be
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-05-22 05:34:24 +0000

    switch to ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import jspm from 'vite-plugin-jspm'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    jspm({
-      defaultProvider: 'esm.sh',
-      integrity: true,
-      inputMap: {
-        imports: {
-          '@artifact/client': 'https://esm.sh/jsr/@artifact/client',
-          '@artifact/client/api': 'https://esm.sh/jsr/@artifact/client/api'
-        }
-      }
-    })
-  ],
-  build: {
-    sourcemap: true
-  },
+  plugins: [react()],
+
+  optimizeDeps: { exclude: ['@privy-io/react-auth', 'lucide-react'] },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
