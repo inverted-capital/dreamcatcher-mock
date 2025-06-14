49f2f820472a2bc4784c58de8668f8a97bba08f2
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-06-14 22:20:40 +1200

    Merge layout components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,28 @@
-import AppLayout from './AppLayout'
 import useHashRouter from './useHashRouter'
 import useHomeScope from '@/shared/useHomeScope'
+import Sidebar from './Sidebar'
+import CanvasContainer from './CanvasContainer'
+import { ChatHistory, ChatInput } from '@/chat'
 
 function App() {
   useHashRouter()
   useHomeScope()
-  return <AppLayout />
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <Sidebar />
+      <main className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 flex overflow-hidden bg-white">
+          <div className="w-2/5 flex flex-col overflow-hidden border-r border-gray-200">
+            <ChatHistory />
+            <ChatInput />
+          </div>
+          <div className="w-3/5 overflow-hidden">
+            <CanvasContainer />
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default App
